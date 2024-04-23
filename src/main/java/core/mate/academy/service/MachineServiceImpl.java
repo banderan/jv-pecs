5a83982781838a3e335767f41e8a61b51440fc4f
Banderan <bartoszwojcik2004@gmail.com>
2024-04-23 22:20:36 +0200

    Start solving the problem

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,35 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T>{
+    @Override
+    public List<T> getAll(Class<? extends T> type) {
+        if(Bulldozer.class == type){
+            BulldozerProducer bulldozerProducer = new BulldozerProducer();
+            return (List<T>) bulldozerProducer.get();
+        } else if (type == Excavator.class){
+            ExcavatorProducer excavatorProducer = new ExcavatorProducer();
+            return (List<T>) excavatorProducer.get();
+        } else {
+            TruckProducer truckProducer = new TruckProducer();
+            return (List<T>) truckProducer.get();
+        }
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        machines.add(value);
+    }
+
+    @Override
+    public void startWorking(List<? extends T> machines) {
+    }
 }
