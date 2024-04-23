162ad563297f80f431b26a762b034e7d628fccff
Banderan <bartoszwojcik2004@gmail.com>
2024-04-23 22:22:09 +0200

    first improve code-style

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -5,7 +5,7 @@ import core.mate.academy.model.Excavator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExcavatorProducer implements MachineProducer<Excavator>{
+public class ExcavatorProducer implements MachineProducer<Excavator> {
     @Override
     public List<Excavator> get() {
         List<Excavator> excavators = new ArrayList<>();
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -9,13 +9,13 @@ import java.util.List;
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl<T extends Machine> implements MachineService<T>{
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
     @Override
     public List<T> getAll(Class<? extends T> type) {
-        if(Bulldozer.class == type){
+        if (Bulldozer.class == type) {
             BulldozerProducer bulldozerProducer = new BulldozerProducer();
             return (List<T>) bulldozerProducer.get();
-        } else if (type == Excavator.class){
+        } else if (type == Excavator.class) {
             ExcavatorProducer excavatorProducer = new ExcavatorProducer();
             return (List<T>) excavatorProducer.get();
         } else {
